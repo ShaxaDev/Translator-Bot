09ec3e863fec07a410de8ce131f29a9271806388
ShaxaDev <69134199+ShaxaDev@users.noreply.github.com>
2020-09-09 11:56:39 +0500

    Update eng.py

--- eng.py
+++ eng.py
@@ -9,7 +9,7 @@ import psycopg2
 
 db = psycopg2 .connect(user='qprulziufzusxo',database='df7rvj9jlqrtq9',password='b249fee9ffb59482fbdecadf685b2312774671c5837e94ed84ddce5ef3b8b4f1',host='ec2-52-87-22-151.compute-1.amazonaws.com',port='5432')
 sql = db.cursor()
-pyscopg2.execute("""CREATE TABLE IF NOT EXISTS user(id BIGINT UNIQUE,ism TEXT)""")
+psycopg2.execute("""CREATE TABLE IF NOT EXISTS user(id BIGINT UNIQUE,ism TEXT)""")
 db.commit()
 
 token = os.environ.get("tg", "1112656267:AAGzzEYPmt4vv16xE97g_9Q2_5czJnUWe3M")
